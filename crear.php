5813980968a53182ef35426bb9bf103265d02644
Lionel Montesdeoca <lionelmontesdeocanuez.com>
2022-10-21 17:38:02 +0100

    connected to bd create/update view, but update not working

--- crear.php
+++ crear.php
@@ -8,46 +8,65 @@
         isset($_GET['update']) ? print_r("<title>Actualizar registro</title>") : print_r("<title>Nuevo registro</title>");
     ?>
     <title>Nuevo registro</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php
     require "enviroment.php";
-    function printForm($nombre = "", $nombreCorto = "", $precio = "", $familia = '', $descripcion = ''){
-        isset($_GET['update']) ? print_r("<form action='update.php' method='post'>") : print_r("<form action='add.php' method='post'>");
-
-        print_r("
-            <fieldset>
-                <legend>Datos del producto</legend>
-                <label for='productName'>Nombre del producto</label>
-                <input type='text' name='productName' id='productName' placeholder='Nombre del producto' value='$nombre'>
-                <label for='shortName'>Nombre corto del producto</label>
-                <input type='text' name='shortName' id='shortName' placeholder='Nombre corto del producto' value='$nombreCorto'>
-                <label for='prize'>Precio €</label>
-                <input type='number' name='prize' id='prize' placeholder='000.00' value='$precio'>
-                <label for='familyProduct'>Familia del producto</label>
-                <select name='familyProduct' id='familyProduct'>
-        ");
-        $result = $GLOBALS['conecction']->query('SELECT * FROM familias');
-        while($family = $result->fetch(PDO::FETCH_OBJ)){
-            if($familia == $family->cod){
-                print_r("
-                <option value='$family->cod' selected>$family->nombre</option>
-            ");
-            }else{
-                print_r("
-                    <option value='$family->cod'>$family->nombre</option>
-                ");
+        function printForm($nombre = "", $nombreCorto = "", $precio = "", $familia = '', $descripcion = ''){
+            $id;
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
             }
+
+            isset($_GET['update']) ? print_r("<form action='crear.php?update=true&id=$id' method='post'>") : print_r("<form action='crear.php' method='post'>");
+
+            print_r("
+                <fieldset>
+                <legend>Datos del producto</legend>
+                    <div class='error' id='nameError'>
+                        El nombre no puede estar vacío.
+                    </div>
+                    <label for='productName'>Nombre del producto</label>
+                    <input type='text' name='productName' id='productName' placeholder='Nombre del producto' value='$nombre'>
+                    <div class='error' id='shortNameError'>
+                        El nombre corto no puede estar vacío.
+                    </div>
+                    <label for='shortName'>Nombre corto del producto</label>
+                    <input type='text' name='shortName' id='shortName' placeholder='Nombre corto del producto' value='$nombreCorto'>
+                    <div class='error' id='prizeError'>
+                        El precio no puede estar vacío o ser 0.
+                    </div>
+                    <label for='prize'>Precio €</label>
+                    <input type='number' name='prize' id='prize' placeholder='000.00' value='$precio'>
+                    <div class='error' id='familyError'></div>
+                    <label for='familyProduct'>Familia del producto</label>
+                    <select name='familyProduct' id='familyProduct'>
+            ");
+            $result = $GLOBALS['conecction']->query('SELECT * FROM familias');
+            while($family = $result->fetch(PDO::FETCH_OBJ)){
+                if($familia == $family->cod){
+                    print_r("
+                    <option value='$family->cod' selected>$family->nombre</option>
+                ");
+                }else{
+                    print_r("
+                        <option value='$family->cod'>$family->nombre</option>
+                    ");
+                }
+            }
+            print_r("
+                    </select>
+                    <div class='error' id='descriptionError'>
+                        La descripción no puede estar vacío.
+                    </div>
+                    <label for='description'>Descripción</label>
+                    <textarea name='description' id='description' cols='30' rows='10' placeholder='Descripción'>$descripcion</textarea>
+                    <input type='submit' value='Enviar' id='submitButton'>
+                </fieldset>
+            </form>
+            ");
         }
-        print_r("
-                </select>
-                <label for='description'>Descripción</label>
-                <textarea name='description' id='description' cols='30' rows='10' placeholder='Descripción'>$descripcion</textarea>
-                <input type='submit' value='Enviar'>
-            </fieldset>
-        </form>
-        ");
-    }
         if (!isset($_GET['id']) && !isset($_GET['update'])) {
             printForm();
         } else{
@@ -58,6 +77,71 @@
             $product = $result->fetch(PDO::FETCH_OBJ);
             printForm($product->nombre,$product->nombre_corto,$product->pvp,$product->familia,$product->descripcion);
         }
+        if ($_POST != "") {
+            print_r($_POST);
+            $nombre = $_POST['productName'];
+            $nombre_corto = $_POST['shortName'];
+            $prize = $_POST['prize'];
+            $familia = $_POST['familyProduct'];
+            $descripcion = $_POST['description'];
+            if(isset($_GET['update'])){
+                echo("update");
+                $id = $_GET['id'];
+                echo $id;
+                $result = $conecction->prepare(
+                    "UPDATE productos
+                    SET
+                    nombre=:nombre,
+                    nombre_corto=:nombre_corto,
+                    pvp=:prize,
+                    familia=:familia,
+                    descripcion=:descripcion,
+                    WHERE id=:id");
+                $data = array(
+                    "nombre"=>$nombre,
+                    "nombre_corto"=>$nombre_corto,
+                    "prize"=>$prize,
+                    "familia"=>$familia,
+                    "descripcion"=>$descripcion,
+                    "id"=>$id
+                );
+                print_r($data);
+                if( $result->execute(array(
+                    "nombre"=>$nombre,
+                    "nombre_corto"=>$nombre_corto,
+                    "prize"=>$prize,
+                    "familia"=>$familia,
+                    "descripcion"=>$descripcion,
+                    "id"=>$id
+                )) )
+                    header("Location: listado.php");
+                else
+                    echo "Error: el producto no ha podido actualizarse correctamente";
+            } else{
+                echo("insert");
+                $result = $conecction->prepare(
+                    "INSERT INTO productos (nombre, nombre_corto, pvp, familia, descripcion)
+                    VALUES (
+                        :nombre,
+                        :nombre_corto,
+                        :prize,
+                        :familia,
+                        :descripcion
+                        )");
+                    $data = array(
+                        "nombre"=>$nombre,
+                        "nombre_corto"=>$nombre_corto,
+                        "prize"=>$prize,
+                        "familia"=>$familia,
+                        "descripcion"=>$descripcion
+                    );
+                if( $result->execute($data) )
+                    header("Location: listado.php");
+                else
+                    echo "Error: el producto no ha podido insertarse correctamente";
+            }
+        }
     ?>
+    <script src="main.js"></script>
 </body>
 </html>
